0d5fde767b9b8dc951e45d703a289c410af13492
musabdev <musabdev65@gmail.com>
2025-04-27 02:20:47 +0500

    fix: close search input when click outside & design the custom scrollbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 // components
 import Searchbar from "./header/Searchbar"
 // icons
@@ -6,10 +6,25 @@ import { Search, Bell } from "lucide-react"
 
 const Header = () => {
   const [isSearch, setIsSearch] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const openSearch = () => {
     setIsSearch(true)
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (inputRef.current && !inputRef.current.contains(event.target as Node)) {
+        setIsSearch(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="w-full bg-transparent flex justify-between px-12 py-4 text-gray-300 -mb-20">
         <div className="flex gap-8">
@@ -19,7 +34,7 @@ const Header = () => {
         </div>
         <div className="flex items-center gap-6">
           {
-            isSearch ? <Searchbar />:
+            isSearch ? <Searchbar inputRef={inputRef} />:
             <Search onClick={openSearch} size={20} className="cursor-pointer"/>
           }
             
--- src/components/header/Searchbar.tsx
+++ src/components/header/Searchbar.tsx
@@ -1,5 +1,5 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
-import { useEffect, useState } from "react";
+import { Ref, useEffect, useState } from "react";
 // component
 import Moviesearch from "./Moviesearch"
 import Loading from "../ui/Loading"
@@ -15,9 +15,10 @@ export type SearchMovie = {
   release_date: string;
 }
 
-const Searchbar = () => {
+const Searchbar = ({ inputRef }: {inputRef: Ref<HTMLInputElement>}) => {
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState<string>("");
+  
 
   const handleRq = async ({ signal }: {signal: AbortSignal | null}): Promise<SearchMovie[]> => {
     const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(searchTerm)}&api_key=${API_KEY}`, { signal })
@@ -35,7 +36,7 @@ const Searchbar = () => {
     };
   }, [searchTerm]);
   
-  const { data, isLoading, error } = useQuery<SearchMovie[]>({
+  const { data, isLoading } = useQuery<SearchMovie[]>({
     queryKey: ['searchMovies', debouncedSearchTerm],
     queryFn: handleRq,
     staleTime: 5 * 60 * 1000,
@@ -43,21 +44,14 @@ const Searchbar = () => {
   });
   
 
-  useEffect(() => {
-    console.log(data)
-  }, [data])
-
   return (
     <div className="relative z-9">
       <div>
-        <input onChange={(e) => setSearchTerm(e.target.value)} type="search" placeholder="Search Movies..." autoCorrect="false" className="outline-none border border-gray-600  px-4 py-2  bg-blackish rounded-t-md"/>
+        <input ref={inputRef} onChange={(e) => setSearchTerm(e.target.value)} type="search" placeholder="Search Movies..." autoCorrect="false" className="outline-none border border-gray-600  px-4 py-2  bg-blackish rounded-t-md"/>
       </div>
       <div className={`w-full ${debouncedSearchTerm.length > 0 ? 'block': 'hidden'} bg-blackish h-96 rounded-b-md absolute border border-gray-700 overflow-y-auto`}>
         {
           isLoading && <Loading />
-        }
-        {
-          error && <p className=" h-full flex items-center justify-center w-[70%] text-center">{error.message}</p>
         }
         {
           data && data?.map(item => <Moviesearch key={item.id} movie={item}/>)
